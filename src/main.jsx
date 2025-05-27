4384051cdd93ccf203c476289020df158a9b385c
Aníbal Landro <alandro@gmail.com>
2025-05-27 00:19:09 -0300

    Fix gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <CarritoProvider>
         <App />
       </CarritoProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
